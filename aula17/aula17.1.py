e4817fd8bd7d12b3b53129127156d78a8b788231
michele <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-09-29 11:30:11 -0300

    aula17

---
+++ aula17/aula17.1.py
@@ -0,0 +1,12 @@
+'''
+Classes: Teclado, Mouse, Monitor e Computador.
+● Classes Teclado, Mouse, Monitor:
+○ Método: __init__ (sem atributos).
+○ Método: ligar() que imprime uma mensagem indicando que o dispositivo está ligado (ex: "O teclado foi ativado.").
+
+● Classe Computador:
+○ Atributos (Composição): teclado, mouse e monitor, que devem ser instâncias das classes correspondentes.
+○ Método: __init__ que inicializa os três atributos.
+○ Método: ligar_computador() que chama o método ligar() de cada um dos seus dispositivos.
+'''
+
