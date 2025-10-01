da714b5b29e45699eca5e7dacb0ceac49e4b45b5
michelechelymodas-wq <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-10-01 11:33:58 -0300

    aula18revisao1escola3.py

---
+++ aula18revisao1.py/escola3.py
@@ -0,0 +1,14 @@
+'''Etapa 3: A Escola (no arquivo escola.py) 
+Uma escola é um lugar que contém muitos estudantes. Ela não é um estudante, mas tem uma 
+coleção deles. Isso é o conceito de Composição. 
+Seu trabalho aqui: 
+● Crie uma classe chamada Escola. 
+● Esta classe deve ter uma lista para guardar todos os objetos Estudante. 
+● Crie um método adicionar_estudante() para colocar novos estudantes na lista da escola. 
+● Crie um método mostrar_relatorio() que percorre a lista de estudantes e imprime todas 
+as suas informações: nome, matrícula, e as notas de cada matéria. 
+
+'''
+class Escola:
+    def__init__(self, nome, idade, motericula):
+    
