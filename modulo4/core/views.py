6d2f62e1b7790beb51c42518456f4c663581eb13
michele <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-05 11:38:38 -0300

    Update views.py

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    context = { 
+        'nome_usuario': 'Júnior', 
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+    }
 
-def inicio(request):
+    return render(request, 'home.html', context)
+
+
+#def inicio(request):
     return HttpResponse("<h1>Olá, Mundo! Esta é minha segunda página Django, que alegria!</h1>")
-
+   
