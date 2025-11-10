8297c387d78c21f10c9a34635db94ea068bf050d
michele <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-10 11:44:32 -0300

    Update views.py

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tarefa
+from .models import Execucao
+   
 
 # Create your views here.
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+    todas_as_tarefas = Execucao.objects.all()
     #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
     context = { 
         'nome_usuario': 'Júnior', 
-        'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+        "tarefas": todas_as_tarefas,
+        "execucoes": todas_as_tarefas,
     }
 
     return render(request, 'home.html', context)
@@ -14,4 +21,5 @@ def home(request):
 
 #def inicio(request):
     return HttpResponse("<h1>Olá, Mundo! Esta é minha segunda página Django, que alegria!</h1>")
+
    
