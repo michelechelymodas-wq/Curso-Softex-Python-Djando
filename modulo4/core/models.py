dcc49cd2735fc2045233a440d8fc351a242534ea
michele <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-10 11:44:43 -0300

    Update models.py

--- modulo4/core/models.py
+++ modulo4/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# Cada classe aqui é "traduzida" para uma tabela no banco de dados
+class Tarefa(models.Model):
+# Um campo de texto curto, com máximo de 200 caracteres
+    titulo = models.CharField(max_length=200)
+    # Um campo booleano (verdadeiro/falso), que por padrão é Falso
+    concluida = models.BooleanField(default=False)
+    # Um campo de data e hora.
+    # auto_now_add=True: Salva a data e hora EXATAMENTE quando o objeto é criado
+    criada_em = models.DateTimeField(auto_now_add=True)
+    # Este é um "método mágico" do Python.
+    # Ele diz ao Django como "chamar" um objeto Tarefa.
+    # Em vez de "Tarefa object (1)", ele mostrará o título da tarefa.
+    # Isso é EXTREMAMENTE útil no painel de administração.
+    def __str__(self):
+     return self.titulo
+
+class Execucao(models.Model):
+    
+    Nome =  models.CharField(max_length=200)
+    local = models.DateTimeField(auto_now_add=True)
+    hora = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+     return self.titulo
+
+
