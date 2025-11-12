a4dad34fb8823f7cea966187031a06c22d7cc62b
michelechelymodas-wq <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-12 11:27:31 -0300

    Update views.py

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tarefa
 from .models import Execucao
+from .forms import TarefaForm
+
+
    
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+
+        if form.is_valid():
+# 5. Salva o objeto no banco de dados!
+            form.save()
+# 6. Redireciona de volta para a 'home'
+# Isso é o Padrão "Post-Redirect-Get" (PRG)
+            return redirect('home')
+# Se o form NÃO for válido, o código continua e
+# o 'form' (com os erros) será enviado para o template
+# 7. Lógica de GET: Se o usuário apenas visitou a página
+    else:
+        form = TarefaForm() # Cria um formulário vazio
+# 8. A busca de dados (fora dos 'ifs', pois é necessária sempre)
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em') # Ordena pelas mais novas
+# 9. Atualize o contexto para incluir o formulário
+    context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'Models', 'Forms'],
+        'tarefas': todas_as_tarefas,
+        'form': form, # 10. Envie o 'form' (vazio ou com erros) para o template
+    }
+    return render(request, 'home.html', context)
+
+
     todas_as_tarefas = Tarefa.objects.all()
-    todas_as_tarefas = Execucao.objects.all()
+    
     #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
     context = { 
         'nome_usuario': 'Júnior', 
