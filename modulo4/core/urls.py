8bbdf93b3e77afb5162bc785ead4080731888cd5
michelechelymodas-wq <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-17 11:20:45 -0300

    Update urls.py

--- modulo4/core/urls.py
+++ modulo4/core/urls.py
@@ -2,15 +2,13 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-    path(
-        '', 
-        views.home,
-        name='home',
-        ), 
+    path('', views.home, name='home'),
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
+]  
 
     # path(
     #     'inicio/',
     #     views.inicio,
     #     name='inicio',
     #   ), 
-] 
