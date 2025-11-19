82487ed46b61284331db40f110e86951f5e2495a
michelechelymodas-wq <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-19 11:57:19 -0300

    Update urls.py

--- modulo4/core/urls.py
+++ modulo4/core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
     path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
+    path('register/', views.register, name='register'), #URL de cadastro
 ]  
 
     # path(
