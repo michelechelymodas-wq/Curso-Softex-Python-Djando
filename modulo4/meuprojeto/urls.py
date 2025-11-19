688d9ac549ca1ad0cc4400d99634361071af7ac7
michelechelymodas-wq <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-19 11:57:50 -0300

    Update urls.py

--- modulo4/meuprojeto/urls.py
+++ modulo4/meuprojeto/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('login/', 
+         auth_views.LoginView.as_view(template_name='login.html', next_page='home'), 
+         name='login'),
+
+    # URL de Logout 
+    # Ela usa a View 'LogoutView'. 'next_page' diz para onde ir após o logout. 
+    path('logout/', 
+         auth_views.LogoutView.as_view(next_page='login'),
+         name='logout'),
 ]
+
