789525fb7bd99cd447e78b699263a48075cc8541
michelechelymodas-wq <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-11-26 11:32:51 -0300

    Update forms.py

--- modulo4/core/forms.py
+++ modulo4/core/forms.py
@@ -1,13 +1,22 @@
 from django import forms
-from .models import Tarefa # Importe o Model
+from .models import Tarefa 
+from projects.models import Project # Importe o Project
+
+# Importe o Model
 # Esta classe herda de 'ModelForm'
 class TarefaForm(forms.ModelForm):
+     def __init__(self, *args, **kwargs):
+         user = kwargs.pop('user', None)
+         super(TarefaForm, self).__init__(*args, **kwargs)  
+         if user:
+            self.fields['project'].queryset = Project.objects.filter(user=user)
+
 # A "mágica" acontece aqui, na classe 'Meta'
-    class Meta:
+     class Meta:
     # 1. Diga ao form qual Model ele deve usar
         model = Tarefa
     # 2. Diga quais campos do Model devem virar campos no form
     # Nós só queremos que o usuário defina o 'titulo'.
     # 'concluida' (default=False) e 'criada_em' (auto_now_add=True)
     # serão preenchidos automaticamente pelo Model.
-        fields = ['titulo']
+        fields = ['titulo','project' ]
