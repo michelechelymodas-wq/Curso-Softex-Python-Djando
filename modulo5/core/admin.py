2ee5afb33ed8618610b706a7c2214b0c22ca8727
michele <226321875+michelechelymodas-wq@users.noreply.github.com>
2025-12-03 11:37:05 -0300

    Create admin.py

---
+++ modulo5/core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Tarefa
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'titulo', 'user', 'concluida', 'criada_em']
+    list_filter = ['concluida', 'criada_em']
+    search_fields = ['titulo', 'user__username']
